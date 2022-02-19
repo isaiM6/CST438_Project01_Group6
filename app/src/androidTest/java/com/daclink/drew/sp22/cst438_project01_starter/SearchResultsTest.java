3d30aa581fdb6147c5431c1d06e18a066dfd3189
halHalberstadt <halberstadtsterling@gmail.com>
2022-02-19 09:03:43 -0800

    added search unit tests

--- app/src/androidTest/java/com/daclink/drew/sp22/cst438_project01_starter/SearchResultsTest.java
+++ app/src/androidTest/java/com/daclink/drew/sp22/cst438_project01_starter/SearchResultsTest.java
@@ -1,37 +1,50 @@
-/**
- * Author: Hal Halberstadt
- * Last Modified: 02/14/2022
- * Abstract: Unit tests for the Search results
- */
-
 package com.daclink.drew.sp22.cst438_project01_starter;
 
+import android.content.Context;
 import android.os.Parcel;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import com.daclink.drew.sp22.cst438_project01_starter.adapters.SearchResultsAdapter;
 import com.daclink.drew.sp22.cst438_project01_starter.models.APIValues;
 import com.daclink.drew.sp22.cst438_project01_starter.models.Search;
+import com.daclink.drew.sp22.cst438_project01_starter.viewModels.SearchViewModel;
+
+import androidx.lifecycle.Observer;
 
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * Class: SearchResultsTest.java
+ * Description: Basic Search results test for the program.
+ * */
+
 public class SearchResultsTest {
 
     // Testing if the user can search for a generic movie and get multiple results
     @Test
-    public void testGenericSearch(){
+    public void testGenericSearch() {
 
     }
 
     @Test
-    public void isSearchparcelable(){
+    public void testEmptySearchSize() {
+
+    }
+
+    @Test
+    public void testSearchSize() {
 
     }
 }
